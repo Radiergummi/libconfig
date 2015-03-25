13a375c02c2ce5b427181d3bb42d1bbde07052cc
Mo <Radiergummi@users.noreply.github.com>
2015-03-25 11:55:31 +0100

    Update config.php

--- config.php
+++ config.php
@@ -137,4 +137,29 @@ class Config {
 	 public function __callStatic($method, $arguments) {
 	 	return $this->$method(implode(', ', $arguments));
 	 }
+	
+	
+	
+	public function offsetSet($offset, $value) {
+		if (is_null($offset)) {
+			$this->data[] = $value;
+		} else {
+			$this->data[$offset] = $value;
+		}
+	}
+
+
+	public function offsetExists($offset) {
+		return isset($this->data[$offset]);
+	}
+
+
+	public function offsetUnset($offset) {
+		unset($this->data[$offset]);
+	}
+
+
+	public function offsetGet($offset) {
+		return isset($this->data[$offset]) ? $this->data[$offset] : null;
+	}
 }
