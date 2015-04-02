e68c49bef094498fe88aedce6e462e1cdd9b7ef8
Mo <Radiergummi@users.noreply.github.com>
2015-04-02 16:52:34 +0200

    Create bootstrap.php

---
+++ tests/bootstrap.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Libconfig Unit Tests Bootstrap File
+ * 
+ */
+
+// $checkFiles['name'] = __DIR__.'/../name.php';
+// $checkFiles[] = __DIR__.'/../vendor/symfony/yaml/Symfony/Component/Yaml/Yaml.php';
+
+// foreach($checkFiles as $file) {
+//     if ( ! file_exists($file)) {
+//         throw new RuntimeException('Install development dependencies to run test suite.');
+//     }
+// }
+
+// $autoload = require_once $checkFiles['autoload'];
+/* EOF: bootstrap.php */
