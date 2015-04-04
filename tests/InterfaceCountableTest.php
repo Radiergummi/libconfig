50ed33f4e0510a61174a80bdcbea2d01aa0d0af7
Mo <Radiergummi@users.noreply.github.com>
2015-04-04 13:07:57 +0200

    Update InterfaceCountableTest.php

--- tests/InterfaceCountableTest.php
+++ tests/InterfaceCountableTest.php
@@ -4,7 +4,7 @@
  * Config Test
  *
  */
-class ConfigTest extends PHPUnit_Framework_TestCase
+class ConfigCountableTest extends PHPUnit_Framework_TestCase
 {
   public function testCountableInterfaceSucceeds()
   {
