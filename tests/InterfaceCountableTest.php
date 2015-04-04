a5e8b11b923cb706e84dc437a8267952bc778e7c
Mo <Radiergummi@users.noreply.github.com>
2015-04-04 13:10:28 +0200

    Update InterfaceCountableTest.php

--- tests/InterfaceCountableTest.php
+++ tests/InterfaceCountableTest.php
@@ -6,10 +6,17 @@
  */
 class ConfigCountableTest extends PHPUnit_Framework_TestCase
 {
-  public function testCountableInterfaceSucceeds()
+  public function testCountableInterface()
   {
     $array = array('a' => 'foo', 'b' => 'bar', 'c' => 'baz');
     $obj = new Radiergummi\libconfig\Config($array);
     $this->assertEquals(3, count($obj));
+  }
+
+  public function testCountableInterfaceWithNestedArray()
+  {
+    $array = array('a' => 'foo', 'b' => 'bar', 'c' => array('sub1' => 'value1', 'sub2' => 'value2', 'sub3' => 'value3'));
+    $obj = new Radiergummi\libconfig\Config($array);
+    $this->assertEquals(3, count($obj, true));
   }
 }
