5ae19c4f8b3516aa9ac8fb29eb5e9df84d4fd69b
Mo <Radiergummi@users.noreply.github.com>
2015-04-04 13:22:28 +0200

    Update and rename InterfaceCountableTest.php to ConfigInterfaceTest.php

---
+++ tests/ConfigInterfaceTest.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * Config Interface Test
+ *
+ * Tests all implemented interfaces
+ */
+class ConfigInterfaceTest extends PHPUnit_Framework_TestCase
+{
+  public function testCountableInterface()
+  {
+    $array = array('a' => 'foo', 'b' => 'bar', 'c' => 'baz');
+    $obj = new Radiergummi\libconfig\Config($array);
+
+    $this->assertEquals(3, count($obj));
+  }
+
+  public function testCountableInterfaceWithNestedArray()
+  {
+    $array = array('a' => 'foo', 'b' => 'bar', 'c' => array('sub1' => 'value1', 'sub2' => 'value2', 'sub3' => 'value3'));
+    $obj = new Radiergummi\libconfig\Config($array);
+
+    $this->assertEquals(3, count($obj, true));
+  }
+  
+  
+  
+  public function testIteratorSucceeds()
+  {
+    $json = '{ "a": "value", "b": ["foo", "bar", "baz"], "c": { "key1": "value1", "key2": ["a", "b", "c"], "key3": { "suba": "valuea", "subb": "valueb", "subc": "valuec" } } }';
+    $obj = new Radiergummi\libconfig\Config($json);
+
+    $actualArr = array();
+    foreach($obj as $key => $val) {
+        $actualArr[$key] = $val;
+    }        
+    $expectedArr = array(
+        'a' => 'value',
+        'b' => array('foo', 'bar', 'baz'),
+        'c' => array(
+          'key1' => 'value1',
+          'key2' => array('a', 'b', 'c'),
+          'key3' => array(
+            'suba' => 'valuea',
+            'subb' => 'valueb',
+            'subc' => 'valuec'
+            )
+        )
+    );
+    $this->assertEquals($expectedArr, $actualArr);
+  }
+}
