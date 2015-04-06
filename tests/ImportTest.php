823413794ff5e67e7f560901f2c7991d71975c63
Mo <Radiergummi@users.noreply.github.com>
2015-04-06 13:12:50 +0200

    Update ImportTest.php

--- tests/ImportTest.php
+++ tests/ImportTest.php
@@ -1,2 +1,72 @@
 <?php
 
+/**
+ * Config Test
+ *
+ */
+class ConfigTest extends PHPUnit_Framework_TestCase
+{
+  private $fixturePath = '';
+
+  function setUp()
+  {
+    parent::setUp();
+    $this->fixturePath = dirname(__FILE__) . '/fixtures/';
+  }
+
+  public function testCreateObjectWithJSONFileAsParameter()
+  {
+    $file = $this->fixturePath . 'json/datatypes.json';
+    $obj = new Radiergummi\Libconfig\Config($file);
+    
+    $this->assertInstanceOf('Radiergummi\Libconfig\Config', $obj);
+  }
+
+  /**
+   * @expectedException Exception
+   */
+  public function testCreateObjectWithMalformedJSONFileAsParameter()
+  {
+    $file = $this->fixturePath . 'json/broken.json';
+    $obj = new Radiergummi\Libconfig\Config($file);
+    
+    $this->assertInstanceOf('Radiergummi\Libconfig\Config', $obj);
+  }
+
+  public function testGetDataFromImportedJSONFile()
+  {
+    $file = $this->fixturePath . 'json/datatypes.json';
+    $obj = new Radiergummi\Libconfig\Config($file);
+    
+    $this->assertEquals('test', $obj->get('a'));
+  }
+
+  public function testCreateObjectWithPHPFileAsParameter()
+  {
+    $file = $this->fixturePath . 'php/datatypes.php';
+    $obj = new Radiergummi\Libconfig\Config($file);
+    
+    $this->assertInstanceOf('Radiergummi\Libconfig\Config', $obj);
+  }
+
+  public function testGetDataFromImportedPHPFile()
+  {
+    $file = $this->fixturePath . 'php/datatypes.php';
+    $obj = new Radiergummi\Libconfig\Config($file);
+    
+    $this->assertEquals('test', $obj->get('a'));
+  }
+
+  /**
+   * @expectedException Exception
+   */
+  public function testCreateObjectWithMalformedPHPFileAsParameter()
+  {
+    $file = $this->fixturePath . 'php/broken.php';
+    $obj = new Radiergummi\Libconfig\Config($file);
+    
+    $this->assertInstanceOf('Radiergummi\Libconfig\Config', $obj);
+  }
+
+
+}
