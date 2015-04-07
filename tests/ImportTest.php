29f2b8de1c795ae41a3c58ebb54e79a52cc30b01
Mo <Radiergummi@users.noreply.github.com>
2015-04-07 09:59:48 +0200

    Update ImportTest.php

--- tests/ImportTest.php
+++ tests/ImportTest.php
@@ -24,15 +24,15 @@ class ImportTest extends PHPUnit_Framework_TestCase
     echo PHP_EOL . PHP_EOL; print_r($obj->get()); echo PHP_EOL . PHP_EOL;
     $this->assertEquals('test', $obj->get('a'));
   }
-/*
+
   public function testCreateObjectWithMalformedJSONFileAsParameter()
   {
     $file = $this->fixturePath . 'json/broken.json';
     $obj = new Radiergummi\Libconfig\Config($file);
     
-    $this->setExpectedException('\Exception');
+    $this->setExpectedException('\RuntimeException');
   }
-*/
+
   public function testCreateObjectWithPHPFileAsParameter()
   {
     $file = $this->fixturePath . 'php/datatypes.php';
