ba57d63d44b6543086ecbd6c2e5d481640124ed4
Mo <Radiergummi@users.noreply.github.com>
2015-04-22 16:06:34 +0200

    Update config.php

--- src/config.php
+++ src/config.php
@@ -97,10 +97,7 @@ class Config extends \ArrayObject
 
           switch(pathinfo($input, PATHINFO_EXTENSION)) {
             case 'php':
-              #ob_start();
               $content = require($input);
-              #$content = ob_get_contents();
-              #print_r('Content: '.$content); die();
               break;
 
             case 'json':
